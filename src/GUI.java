e35e93d3e169e70c352f09165525cfcc9543dbfe
TiagoLealPalma <tiagolealpalma@gmail.com>
2024-10-15 08:11:27 +0100

    Wordle Solver
    Solving done.
    - Optimizations needed
    - Code Cleanup

--- src/GUI.java
+++ src/GUI.java
@@ -1,25 +1,46 @@
 import Solver.Square;
+import Solver.WordleSolver;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+
+import static java.awt.Color.white;
 
 public class GUI extends JPanel implements ActionListener {
 
-    private Timer timer;
+    private WordleSolver solver;
     private ArrayList<Square> squares = new ArrayList<>();
     private JPanel panel;
     private Point mousePointer;
+    private Timer timer;
     private int initialX = 400;
     private int initialY = 50;
+    public boolean started = false;
 
     public GUI(JPanel panel){
         this.panel = panel;
         for (int i = 0; i < 30; i++) {
             squares.add(new Square(true));
         }
+        try {
+            solver = new WordleSolver(squares);
+            solver.start();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        timer = new Timer(50, this);
+    }
 
+    private void buildUi() {
+
+    }
+
+    private void start() {
+        timer.start();
+        repaint();
     }
 
     @Override
@@ -53,7 +74,7 @@ public class GUI extends JPanel implements ActionListener {
         int spacing = 10;
 
         Graphics2D g2d = (Graphics2D) g;
-
+        g.setFont(new Font("Arial", Font.BOLD, 80));
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 5; j++) {
 
@@ -64,7 +85,7 @@ public class GUI extends JPanel implements ActionListener {
                                 g2d.setColor(Color.GRAY);
                                 break;
                             case YELLOW:
-                                g2d.setColor(Color.YELLOW);
+                                g2d.setColor(Color.orange);
                                 break;
                             case GREEN:
                                 g2d.setColor(Color.GREEN);
@@ -73,69 +94,86 @@ public class GUI extends JPanel implements ActionListener {
                                 break;
                         }
                         g.fillRect(initialX + 100*j + spacing, initialY + 100 * i + spacing, 80, 80);
+                        g.setColor(white);
+                        g.drawString(square.getLetter().toUpperCase(), initialX + 12 + 100 * j + spacing, initialY + 70 + 100 * i + spacing);
                     }
-                g.setColor(Color.black);
-                g.drawRect(initialX + 100*j + spacing, initialY + 100 * i + spacing, 80, 80);
+                    else {
+                        g.setColor(Color.black);
+                        g.drawRect(initialX + 100 * j + spacing, initialY + 100 * i + spacing, 80, 80);
+                        if(square.getLetter() != null)
+                            g.drawString(square.getLetter().toUpperCase(), initialX + 12 + 100 * j + spacing, initialY + 70 + 100 * i + spacing);
+                    }
             }
         }
     }
 
     public static void main(String[] args) {
-    // Frame
-    JFrame frame = new JFrame("Physics Simulator");
-    frame.setLayout(new BorderLayout());
+        // Frame
+        JFrame frame = new JFrame("Physics Simulator");
+        frame.setLayout(new BorderLayout());
 
-    // Panel
-    JPanel panel = new JPanel();
-    panel.setPreferredSize(new Dimension(1300, 700));
-    panel.setLayout(new BorderLayout());
+        // Panel
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(1300, 700));
+        panel.setLayout(new BorderLayout());
 
-    // Button
-    JButton calculate = new JButton("Calculate");
+        // Button
+        JButton calculate = new JButton("Calculate");
+        boolean started = false;
 
-    // Add stuff
-    frame.add(calculate, BorderLayout.SOUTH);
-    frame.add(panel, BorderLayout.CENTER);  // Add the panel to the frame
+        // Add stuff
+        frame.add(calculate, BorderLayout.SOUTH);
+        frame.add(panel, BorderLayout.CENTER);  // Add the panel to the frame
 
-    // Adjust sizes
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.pack();
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
+        // Adjust sizes
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
-    // Create the GUI instance before the listener, so we can access its engine
-    GUI gui = new GUI(panel);
+        // Create the GUI instance before the listener, so we can access its engine
+        GUI gui = new GUI(panel);
 
-    // MouseListener added directly to panel
-    panel.addMouseListener(new MouseListener() {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            gui.clickedSquare(e.getPoint());
-            gui.repaint();}
+        // MouseListener added directly to panel
+        panel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                gui.clickedSquare(e.getPoint());
+                gui.repaint();}
 
-        @Override
-        public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mousePressed(MouseEvent e) {}
 
-        @Override
-        public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mouseReleased(MouseEvent e) {}
 
-        @Override
-        public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
 
-        @Override
-        public void mouseExited(MouseEvent e) {}
-    });
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
 
-    // Listener for the start button (So the GUI can start with proper sizes)
-    calculate.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            panel.add(gui, BorderLayout.CENTER);
-            panel.revalidate();
-            panel.repaint();
-            gui.repaint();
-        }
-    });
+        // Listener for the start button (So the GUI can start with proper sizes)
+        calculate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!gui.started) {
+                    panel.add(gui, BorderLayout.CENTER);
+                    panel.revalidate();
+                    panel.repaint();
+                    gui.repaint();
+                    gui.start();
+                    gui.started = true;
+                }else{
+                    gui.solver.incrementCurrentAttempt();
+                    gui.solver.notifyInput();
+                }
+
+            }
+        });
     }
+
+
 
 }
--- src/Solver/WordleSolver.java
+++ src/Solver/WordleSolver.java
@@ -5,7 +5,9 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class WordleSolver {
+import static java.lang.Thread.sleep;
+
+public class WordleSolver extends Thread {
     private class Letter{
         private char letter;
         private double occurrence;
@@ -31,6 +33,9 @@ public class WordleSolver {
     private ArrayList<String> dataBase;
     private final char[] alphabet;
     private ArrayList<Letter> letters;
+    private int currentAttempt = 0;
+    private int lastAttempt = 0;
+    private boolean solved = false;
 
     public WordleSolver(ArrayList<Square> squares) throws FileNotFoundException {
         this.squares = squares;
@@ -66,35 +71,123 @@ public class WordleSolver {
         }
     }
 
-    public void solve(){
-        getFirstWord();
-        // get the squares to correspond next unoccupied squares to receive the solved word
+    @Override
+    public void run() {
+        solve();
+    }
+
+    public synchronized void solve(){
+        addToBoard(getFirstWord());
+        while(!solved){
+            while(currentAttempt == lastAttempt) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            lastAttempt = currentAttempt;
+            System.out.println(getWord());
+            addToBoard(getWord());
+        }
+
+
+
+
+
+    }
+
+    // Front-Back logic
+    private void addToBoard(String word){
+        int index = 0;
+        for (int i = (currentAttempt*5) + 0; i < (currentAttempt*5) + 5; i++) {
+            squares.get(i).setLetter(String.valueOf(word.charAt(index)));
+            index++;
+        }
     }
 
 
+    // Solving logic
+
+    // Calculate what is the summed occurrence of all the letters in the same word, since we have no other information.
+    // Note: Take out words that repeated letters
     private String getFirstWord(){
-        // Calculate what is the summed occurrence of all the letters in the same word, since we have no other information.
-        // Note: Take out words that repeated letters
-        String firstword = "faile";
+        String firstword = "";
         double value = -1.0;
         for(String str : dataBase){
-            if(value < summedOccurence(str)){
+            if(value < summedOccurence(str, false)){
                 firstword = str;
-                value = summedOccurence(str);
+                value = summedOccurence(str, false);
             }
         }
-
         return firstword;
     }
 
-    private double summedOccurence(String word){
+    private String getWord(){
+        // Get lasts word values, returned by the user.
+        Square[] lastWord = new Square[5];
+        for (int i = 0; i < 5; i++) {
+            lastWord[i] = squares.get((currentAttempt-1)*5 + i);
+        }
+
+        boolean skip = false;
+        ArrayList<String> newDataBase = new ArrayList<>();
+        // Run through array handling each letter value
+        for(String str : dataBase){
+            for (int pos = 0; pos < 5; pos++) {
+                char letter = lastWord[pos].getLetter().charAt(0);
+
+                if (lastWord[pos].getValue() == Square.Value.GREEN) {
+                    if (str.charAt(pos) != letter) {
+                        skip = true;
+                        break;
+                    }
+                }
+
+                if (lastWord[pos].getValue() == Square.Value.YELLOW) {
+                    if (str.indexOf(letter) == -1 || str.charAt(pos) == letter) {
+
+                        skip = true;
+                        break;
+                    }
+                }
+
+                if (lastWord[pos].getValue() == Square.Value.GRAY) {
+                    if (str.indexOf(letter) != -1) {
+                        skip = true;
+                        break;
+                    }
+                }
+            }
+            if(!skip) newDataBase.add(str);
+            else skip = false;
+        }
+
+        dataBase = newDataBase;
+        String newWord = null;
+        boolean allowRepeatedWords = false;
+        while(newWord == null){
+            double value = -1.0;
+
+            for(String str : dataBase) {
+                if (value < summedOccurence(str, allowRepeatedWords)) {
+                    newWord = str;
+                    value = summedOccurence(str, allowRepeatedWords);
+                }
+            }
+            allowRepeatedWords = true;
+        }
+        return newWord;
+    }
+
+    private double summedOccurence(String word, boolean allowRepeatedLetters){
         if(word.length() != 5) throw new IllegalArgumentException();
 
         char[] array = word.toCharArray();
         double occurrence = 0.0;
 
         for (int i = 0; i < 5; i++) {
-            if(word.indexOf(array[i]) != i){
+            if(word.indexOf(array[i]) != i && !allowRepeatedLetters){
                 return -1.0;
             }
             occurrence += letters.get(array[i]-'a').getOccurrence();
@@ -102,6 +195,17 @@ public class WordleSolver {
         return occurrence;
     }
 
+    public synchronized void notifyInput(){
+        notifyAll();
+    }
+
+    public int getCurrentAttempt(){
+        return currentAttempt;
+    }
+
+    public void incrementCurrentAttempt(){
+        currentAttempt++;
+    }
 
 
 
@@ -129,12 +233,12 @@ public class WordleSolver {
 
 
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         ArrayList<Square> squares = new ArrayList<>();
         WordleSolver solver = new WordleSolver(squares);
 
-
-        System.out.println(solver.getFirstWord());
+        sleep(500);
+        solver.solve();
 
     }
 }
